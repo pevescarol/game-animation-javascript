745c1182ee29485ee5834fd21f75748a5664d234
pevescarol <carol.peves@gmail.com>
2020-05-08 15:59:38 -0300

    primer version del juego, solo js

---
+++ piedra-papel-tijera.js
@@ -0,0 +1,83 @@
+const getUserChoice = userInput => {
+   userInput = userInput.toLowerCase();
+
+   if(userInput ==='rock'|| userInput ==='paper' || userInput ==='scissors' ){
+     return userInput;
+   }else{
+     console.log("Error!");
+   }
+};
+
+//console.log(getUserChoice('papel'));
+
+function getComputerChoice(){
+  randomN = Math.floor(Math.random()*3);
+  switch(randomN){
+    case 0:
+          return 'rock';
+    break;
+    case 1:
+          return 'paper';
+    break;
+    case 2:
+          return 'scissors';
+    break;
+  }
+}
+
+//console.log(getComputerChoice());
+
+function determineWinner(userChoice, computerChoice){
+   if(userChoice===computerChoice){
+     return 'The game is a tie';
+   }
+   if (userChoice==='rock'){
+     if(computerChoice==='paper'){
+       return 'The computer won!'
+     }else{
+       return 'You won!';
+     }
+   }
+   if(userChoice==='paper'){
+     if(computerChoice==='scissors'){
+        return 'The computer won!';
+     }else{
+       return 'you won!';
+     }
+    }
+    if(userChoice==='paper'){
+     if(computerChoice==='rock'){
+        return 'You won!';
+     }else{
+       return 'The computer won!';
+     }
+    }
+
+   if(userChoice==='scissors'){
+     if(computerChoice==='rock'){
+       return 'The computer won';
+     }else{
+       return 'You win';
+     }
+   }
+    if(userChoice==='scissors'){
+     if(computerChoice==='paper'){
+       return 'You won';
+     }else{
+       return 'The computer won';
+     }
+   }
+ }
+  // console.log(determineWinner('scissors', 'paper'));
+
+  const playGame = () => {
+    const userChoice  = getUserChoice('scissors');
+    const computerChoice =
+     getComputerChoice();
+     console.log('You threw: ' + userChoice);
+     console.log('The computer threw: ' + computerChoice);
+     console.log(determineWinner(userChoice, computerChoice));
+  };
+
+  playGame();
+  
